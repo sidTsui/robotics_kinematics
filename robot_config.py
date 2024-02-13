2a0d5c1ea282926c9230f0e7f6532dda49f88906
sidTsui <smt4601@uncw.edu>
2024-02-13 16:38:38 +0000

    lab3 outline

---
+++ robot_config.py
@@ -0,0 +1,9 @@
+###Sidney Tsui
+###Lab3-robot kinematics
+###problem 2
+###2/13/24
+
+###Purpose: test robot_model.py
+
+import robot_model.py
+
---
+++ robot_model.py
@@ -0,0 +1,29 @@
+###Sidney Tsui
+###Lab3-robot kinematics
+###problem 1
+###2/13/24
+
+
+
+###this function:
+#######receives the Denavit-Hartenberg parameters
+#######returns a combined homogenous transformation according to this convention.
+def dh_transformation:
+	###contents
+	
+	
+###this function:
+#######receives a 2D list/array containing the DH parameters of a robotic manipulator and returns a homogenous transformation for the kinematic chain
+def kinematic_chain:
+	###set up a loop that scans the rows and multiplies the transformation 			to get a total transformation.
+	
+###this function:
+#######eceives a homogeneous transformation as input and returns the x, y, z components of the position.
+def get_pos:
+	###contents in assignment sheet
+	
+
+###this function:
+#######eceives a homogeneous transformation as input and returns roll-pith-yaw angles 
+def get_rot:
+	###order for rotational motion in assignment sheet
